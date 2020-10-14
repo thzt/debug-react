dd253350490a7f9d063842b69b967dba6d70f9e9
thzt <thzt.xx@gmail.com>
2020-10-14 17:32:42 +0800

    feat: class component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,40 @@
 import React, { useState } from 'react';
 import './App.css';
 
+// class App extends React.Component {
+//   state = {
+//     v: 0,
+//   };
+
+//   increment = () => {
+//     debugger;
+//     this.setState(s => {
+//       debugger;
+//       return {
+//         v: s.v + 1,
+//       };
+//     });
+//     debugger;
+//     this.setState(s => {
+//       debugger;
+//       return {
+//         v: s.v + 2,
+//       };
+//     });
+//     debugger;
+//   };
+
+//   render = () => {
+//     const {
+//       increment,
+//       state: { v },
+//     } = this;
+
+//     debugger;
+//     return <button onClick={increment}>{v}</button>;
+//   };
+// }
+
 function App() {
   debugger;
   const [state, setState] = useState(0);
